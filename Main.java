47a9ba1dad1907b80cb8742230ae32a7d88ad40d
Maksym Biriukov <birykovms2003@gmail.com>
2021-10-15 18:53:20 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public  class  Main{
+    public  static  void  Main(String [] args) {
+        System.out.println("Hello  world!");
+        System.out.println("Hello Abobys :)");
+        int a;
+        a = 5;
+        int b;
+        b = 18;
+        int c = a + b;
+        System.out.println(c);
+
+        }
+        }
